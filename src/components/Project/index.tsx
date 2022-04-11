8f0fa37db3c8d989bc937fdc6c4f911576ddc3fe
Lucas Andrade <lucasantos.dev@gmail.com>
2022-04-11 19:03:28 -0300

    create project component

---
+++ src/components/Project/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { Container, ProjectTitle, ProjectDescription } from './styles'
+
+const Project = () => {
+	return (
+		<Container>
+			<ProjectTitle>
+        Project Test
+			</ProjectTitle>
+      
+			<ProjectDescription>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione est maiores cupiditate!
+			</ProjectDescription>
+		</Container>
+	)
+}
+
+export default Project
---
+++ src/components/Project/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 30%;
+  background-color: #FFF;
+  padding: 1rem;
+  border-radius: 5px;
+  box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.15);
+  margin-bottom: 1rem;
+`
+
+export const ProjectTitle = styled.h2`
+  font-weight: normal;
+  margin-bottom: 1rem;
+`
+
+export const ProjectDescription = styled.p`
+  font-size: 0.875rem;
+`
