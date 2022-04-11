1e6bcd5c01a33ebca6cf69b20897ae94f0b17b78
Lucas Andrade <lucasantos.dev@gmail.com>
2022-04-11 16:10:46 -0300

    update global style

--- src/global/globalStyle.ts
+++ src/global/globalStyle.ts
@@ -1,6 +1,12 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyled = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  
   body {
     background-color: #EEE;
     font-family: 'Poppins', sans-serif;
