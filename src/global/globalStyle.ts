91ae5674609e59d9bbdfee443cdc5ce4c68bf2a8
Lucas Andrade <lucasantos.dev@gmail.com>
2022-04-11 15:40:11 -0300

    configure global style

---
+++ src/global/globalStyle.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyled = createGlobalStyle`
+  body {
+    background-color: #EEE;
+    font-family: 'Poppins', sans-serif;
+  }
+`
+
+export default GlobalStyled
