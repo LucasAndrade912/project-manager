b359a68ff179d5f3347cd9f841bd2d6569c674f1
Lucas Andrade <lucasantos.dev@gmail.com>
2022-04-14 23:29:50 -0300

    implements logic for redirect

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 
 import { Container } from './styles'
 import { Menu } from '../../components'
 
 const App = () => {
+	const { pathname } = useLocation()
+	const navigate = useNavigate()
+
+	useEffect(() => {
+		if (pathname === '/app') {
+			navigate('projects')
+		}
+	}, [])
+
 	return (
 		<Container>
 			<Menu />
