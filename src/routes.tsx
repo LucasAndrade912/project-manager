164b75b746c713c9e451c4768bb3d2d65745d44a
Lucas Andrade <lucasantos.dev@gmail.com>
2022-05-17 21:05:36 -0300

    add react query

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 import { App, Login, Project, Projects } from './pages'
 
+const queryClient = new QueryClient()
+
 const routes = () => {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Login />} />
-				<Route path="/app" element={<App />}>
-					<Route path="/app/projects" element={<Projects />} />
-					<Route path="/app/project" element={<Project />} />
-				</Route>
-			</Routes>
-		</BrowserRouter>
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Login />} />
+					<Route path="/app" element={<App />}>
+						<Route path="/app/projects" element={<Projects />} />
+						<Route path="/app/project" element={<Project />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</QueryClientProvider>
 	)
 }
 
