aa50396b9a51a3a0526e5bd6011477d6a636b20f
Lucas Andrade <lucasantos.dev@gmail.com>
2022-05-18 21:59:56 -0300

    remove react query

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,24 +1,19 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from 'react-query'
 
 import { App, Login, Project, Projects } from './pages'
 
-const queryClient = new QueryClient()
-
 const routes = () => {
 	return (
-		<QueryClientProvider client={queryClient}>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<Login />} />
-					<Route path="app" element={<App />}>
-						<Route path="projects" element={<Projects />} />
-						<Route path="project" element={<Project />} />
-					</Route>
-				</Routes>
-			</BrowserRouter>
-		</QueryClientProvider>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Login />} />
+				<Route path="app" element={<App />}>
+					<Route path="projects" element={<Projects />} />
+					<Route path="project" element={<Project />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
 	)
 }
 
