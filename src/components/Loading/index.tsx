ce6e53f10ac770330ed421c4c08ef5d1fe2d3948
Lucas Andrade <lucasantos.dev@gmail.com>
2022-08-15 14:43:46 -0300

    update Loading component to update default color

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 import { Spinner } from './styles'
 
-const Loading = ({ size }:  { size: string }) => {
-	return <Spinner size={size} />
+interface LoadingProps {
+	size: string
+	color?: string
+}
+
+const Loading = ({ size, color }:  LoadingProps) => {
+	return <Spinner size={size} color={color} />
 }
 
 export default Loading
--- src/components/Loading/styles.ts
+++ src/components/Loading/styles.ts
@@ -16,9 +16,23 @@ export const Spinner = styled.div`
 	}};
   height: ${(props: { size: string }) => {
 		return props.size
-	}};;
-  border: 3px solid rgba(0, 0, 0, 0.8);
-  border-color: rgba(0, 0, 0, 0.8) transparent rgba(0, 0, 0, 0.8) transparent;
+	}};
+  border: ${(props: { color?: string }) => {
+		let borderColor = 'rgba(0, 0, 0, 0.8)'
+
+		if (props.color) {
+			borderColor = props.color
+		}
+
+		return `3px solid ${borderColor}`
+	}};
+  border-color: ${(props: { color?: string }) => {
+		if (props.color) {
+			return `${props.color} transparent ${props.color} transparent`
+		}
+
+		return 'rgba(0, 0, 0, 0.8) transparent rgba(0, 0, 0, 0.8) transparent'
+	}};
   border-radius: 50%;
   animation: ${rotate} 1s linear infinite;
 `
